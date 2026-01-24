ae9e5be8c83979530c9e9d8b6b39c1cfa9ce1dcd
Pratham4403 <mohiteprathamesh2005@gmail.com>
2026-01-24 19:43:15 +0530

    Implemented useEffect hook example

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [likes, setLikes] = useState(0);
   const [dislikes, setDislikes] = useState(0);
 
-  return (
-    <div>
-      <h2>👍 Likes: {likes} <br/>👎 Dislikes: {dislikes}</h2>
+  const [notifications, setNotifications] = useState(0);
+  useEffect(() => {
+    if (notifications > 0) {
+      document.title = `(${notifications}) New Notifications`;
+    } else {
+      document.title = "No New Notifications";
+    }
+  }, [notifications]);      //runs when notifications change
 
-      <button onClick={() => setLikes(likes + 1)}>
-        Like
-      </button>
-      <button onClick={() => setDislikes(dislikes + 1)}>
-        Dislike
-      </button>
-    </div>
+
+  return (
+    <>
+      <div>
+        <h2>👍 Likes: {likes} <br/>👎 Dislikes: {dislikes}</h2>
+
+        <button onClick={() => setLikes(likes + 1)}>
+          Like
+        </button>
+        <button onClick={() => setDislikes(dislikes + 1)}>
+          Dislike
+        </button>
+      </div>
+      <div>
+        <h2>Notifications: {notifications}</h2>
+        <button onClick={() => setNotifications(notifications + 1)}>
+          Add Notification
+        </button>
+        <button onClick={() => setNotifications(0)}>
+          Clear Notifications
+        </button>
+      </div>
+    </>
   );
 }
 
