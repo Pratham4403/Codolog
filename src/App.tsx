155decb1b58200b73fbcdac51144e3ed0bb16ee7
Pratham4403 <mohiteprathamesh2005@gmail.com>
2026-01-24 19:17:13 +0530

    Implemented useState hook example

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
 
   return (
-    <>
-      <div>
-        <h1>Hello from Prathamesh !</h1>
-      </div>
-    </>
-  )
+    <div>
+      <h2>👍 Likes: {likes} <br/>👎 Dislikes: {dislikes}</h2>
+
+      <button onClick={() => setLikes(likes + 1)}>
+        Like
+      </button>
+      <button onClick={() => setDislikes(dislikes + 1)}>
+        Dislike
+      </button>
+    </div>
+  );
 }
 
 export default App
